4beb2ae78c420669162d5af7ee707201c13787fd
essenciademulhermake <essenciademulher.make@gmail.com>
2026-09-19 19:22:18 -0300

    Adiciona service worker PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="deumclick-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./style.css","./app.js"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
